2c34c857ffa6cc08951d049177e1e6464247bc6e
Han Lin <hotdogee@gmail.com>
2019-10-31 21:08:58 +0800

    training goa v2

--- goa-v2.py
+++ goa-v2.py
@@ -3778,23 +3778,23 @@ class DecoderLayer(tf.keras.layers.Layer):
 
         attn1, attn_weights_block1 = self.mha1(
             inputs, inputs, inputs, look_ahead_mask
-        )  # (batch_size, target_seq_len, d_model)
+        ) # (batch_size, target_seq_len, d_model)
         attn1 = self.dropout1(attn1, training=training)
         out1 = self.layernorm1(attn1 + inputs)
 
         attn2, attn_weights_block2 = self.mha2(
             enc_output, enc_output, out1, padding_mask
-        )  # (batch_size, target_seq_len, d_model)
+        ) # (batch_size, target_seq_len, d_model)
         attn2 = self.dropout2(attn2, training=training)
         out2 = self.layernorm2(
             attn2 + out1
-        )  # (batch_size, target_seq_len, d_model)
+        ) # (batch_size, target_seq_len, d_model)
 
         ffn_output = self.ffn(out2)  # (batch_size, target_seq_len, d_model)
         ffn_output = self.dropout3(ffn_output, training=training)
         out3 = self.layernorm3(
             ffn_output + out2
-        )  # (batch_size, target_seq_len, d_model)
+        ) # (batch_size, target_seq_len, d_model)
 
         return out3, attn_weights_block1, attn_weights_block2
 
